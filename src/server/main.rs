08ec2537cabcd5aa92c2b3f8398108ac88cc19a3
eignnx <eignnx@gmail.com>
2018-05-02 15:33:24 -0400

    Use websockets to serve clients, impl threads

--- src/server/main.rs
+++ src/server/main.rs
@@ -8,14 +8,16 @@ extern crate serde_json;
 #[macro_use]
 extern crate dotenv_codegen;
 
-use std::io::Write;
-use std::net::{SocketAddrV4, TcpListener, TcpStream};
-use std::sync::Arc;
+extern crate websocket;
+
+use std::net::{SocketAddrV4, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 use chrono::{DateTime, UTC};
-use serde::{Serialize, Deserialize};
-use serde_json::{Serializer, Deserializer};
 
+use websocket::server::{NoTlsAcceptor};
+use websocket::OwnedMessage::{Text, Close};
 
 #[derive(Debug, Eq, PartialEq, Hash)]
 pub struct ConnectedClient {
@@ -31,50 +33,77 @@ struct ChatMessage {
     body: String
 }
 
-#[derive(Debug)]
 pub struct ChatServer {
-    listener: TcpListener,
-    chat_history: Arc<Vec<ChatMessage>>,
+    listener: websocket::sync::Server<NoTlsAcceptor>,
+    chat_history: Arc<Mutex<Vec<ChatMessage>>>,
 }
 
 impl ChatServer {
     pub fn new() -> Self {
-        let address = dotenv!("MIN_CHAT_SERVER_ADDRESS").parse::<SocketAddrV4>().unwrap();
-        let listener = TcpListener::bind(address).unwrap();
-        ChatServer { listener, chat_history: Arc::new(Vec::new()) }
+        let address = dotenv!("MIN_CHAT_SERVER_ADDRESS").parse::<SocketAddrV4>()
+            .expect("Could not parse server address");
+        let listener = websocket::sync::Server::bind(address).unwrap();
+        ChatServer { listener, chat_history: Arc::new(Mutex::new(Vec::new())) }
     }
 
     pub fn run(self) {
         println!("Listening on {:?}", self.listener.local_addr().unwrap());
-        for mut stream in self.listener.incoming() {
-            let mut stream = stream.unwrap();
-            println!("Accepted connection on {:?}", stream.local_addr().unwrap());
-
-            let mut msg = b"Hello is this working?\n";
-            stream.write_all(&msg[..]);
-
-            // Listen to stream, get username of connecting client.
-            // let username: String = serde_json::from_reader(stream).unwrap();
-            // let new_user = ConnectedClient {username, id: 12345};
-
-            // // TODO: Put inside separate thread!
-            // let handler = ClientHandler {
-            //     server: self.chat_history.clone(), // Bumps reference count.
-            //     stream
-            // };
-
-            // ...
+        for connection in self.listener.filter_map(Result::ok) {
+            let chat_history = self.chat_history.clone();
+            thread::spawn(move || {
+                let mut client = ClientHandler {
+                    history: chat_history,
+                    stream: connection.accept().unwrap()
+                };
+                println!("Accepted connection on {:?}", client.stream.local_addr().unwrap());
+                client.run();
+            });
         }
     }
 }
 
-#[derive(Debug)]
 pub struct ClientHandler {
-    server: Arc<Vec<ChatMessage>>,
-    stream: TcpStream,
+    history: Arc<Mutex<Vec<ChatMessage>>>,
+    stream: websocket::sync::Client<TcpStream>,
+}
+
+impl ClientHandler {
+    pub fn send_all_history(&mut self) {
+        for chat_msg in self.history.lock().unwrap().iter() {
+            let string = serde_json::to_string(&chat_msg).unwrap();
+            let msg = websocket::Message::text(string);
+            self.stream.send_message(&msg).unwrap();
+        }
+    }
+
+
+    pub fn run(&mut self) {
+        self.send_all_history();
+        loop {
+            let msg = match self.stream.recv_message().unwrap() {
+                Text(txt) => txt,
+                Close(_) => return,
+                _ => panic!("Recieved uninterpretable message!")
+            };
+
+            let chat_message: ChatMessage = serde_json::from_str(&msg).unwrap();
+            println!("Recv'd {:?} from client", chat_message);
+            let mut history = self.history.lock().unwrap();
+            history.push(chat_message);
+        }
+    }
 }
 
 fn main() {
     let server = ChatServer::new();
+    {
+        let mut history = server.chat_history.lock().unwrap();
+        history.push(ChatMessage {
+            sender: "User1".to_owned(), body: "Hi there, how's it going?".to_owned(), timestamp: 1234123412
+        });
+        history.push(ChatMessage {
+            sender: "User2".to_owned(), body: "It's going well!".to_owned(), timestamp: 1234123612
+        });
+    }
     server.run();
 }
