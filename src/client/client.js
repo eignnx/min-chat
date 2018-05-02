be74c86a8d565404bea96647730cce5c776b97a3
eignnx <eignnx@gmail.com>
2018-05-02 13:54:26 -0400

    Impl client first draft

---
+++ src/client/client.js
@@ -0,0 +1,55 @@
+
+const KNOWN_SERVER_ADDRESS = "ws:127.0.0.1:9090";
+const PROTOCOL = "MinChatProtocol";
+
+let websocket = new WebSocket(KNOWN_SERVER_ADDRESS);
+let username = "TestUser123";
+
+class ChatMessage {
+    constructor(user, body, timestamp) {
+        this.user = user;
+        this.body = body;
+        this.timestamp = timestamp;
+    }
+
+    static fromJSON(obj) {
+        return new ChatMessage(obj.user, obj.body, obj.timestamp);
+    }
+
+    transmit(websocket) {
+        websocket.send(JSON.stringify(this));
+    }
+
+    toHTML() {
+        return `
+            <div class="row my-5">
+                <div class="card">
+                    <div class="card-header">
+                        ${this.user}
+                    </div>
+                    <div class="card-body">
+                        ${this.body}
+                    </div>
+                    <div class="card-footer text-muted">
+                        ${new Date(this.timestamp).toLocaleString()}
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
+
+function sendTextboxChatMessage() {
+    let msg = new ChatMessage(
+        username,
+        document.getElementById("chat-text-box").value,
+        Date.now()
+    );
+    msg.transmit(websocket);
+}
+
+websocket.onmessage = (event) => {
+    let msg = ChatMessage.fromJSON(JSON.parse(event.data));
+    let window = document.getElementById("chat-history-window");
+    window.innerHTML += msg.toHTML();
+}
